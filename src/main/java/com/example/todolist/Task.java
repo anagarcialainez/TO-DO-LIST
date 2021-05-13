a8b7e89704482e15b4517a3e0aa17a75dfb9a5e5
Gaby8909 <gabi_oa@hotmail.com>
2021-05-13 14:18:26 +0200

    falllos

--- src/main/java/com/example/todolist/Task.java
+++ src/main/java/com/example/todolist/Task.java
@@ -1,18 +1,5 @@
 package com.example.todolist;
 
-public class Task {
-    private final String Tareas;
 
-    public Task(String)
-    {
-        this.Tareas = Tareas;
-            }
-
-    public String getTitle() {
-        return Tareas;
-    }
-
-
-}
 
 
