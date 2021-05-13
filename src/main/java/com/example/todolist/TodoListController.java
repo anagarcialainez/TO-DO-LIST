087ef60d7d48c8fc6bbc262a78bbbbd3f0d51790
Gaby8909 <gabi_oa@hotmail.com>
2021-05-13 13:01:01 +0200

    cambios

--- src/main/java/com/example/todolist/TodoListController.java
+++ src/main/java/com/example/todolist/TodoListController.java
@@ -2,8 +2,10 @@ package com.example.todolist;
 
 import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -11,14 +13,16 @@ public class TodoListController {
     private List<Task> TodoList ;
 
     public TodoListController() {
-        TodoList = List.of(
-                new Task("estudiar java"),
-                new Task("practicar todos los dias"),
-                new Task("suicidarse")
-        );
+        TodoList = new ArrayList<>();
+
     }
-}
+
 @GetMapping ("/TodoList")
 public List<Task> allTodoList() {
     return TodoList;
 }
+    @PostMapping("/TodoList")
+    public RedirectView addNewTask(Task Task) {
+        TodoList.add(Task);
+        return new RedirectView("/");
+    }
