ba94e4c58dbf5b9e62993bb072c69aa62f8db342
anagarcialainez <accgl@hotmail.com>
2021-05-19 13:01:10 +0200

    RestController

--- src/main/java/com/example/todolist/Task.java
+++ src/main/java/com/example/todolist/Task.java
@@ -5,6 +5,7 @@ public class Task {
     private final String task;
 
     public Task(String task){
+
         this.task = task;
 }
 
--- src/main/java/com/example/todolist/TaskController.java
+++ src/main/java/com/example/todolist/TaskController.java
@@ -1,4 +1,7 @@
 package com.example.todolist;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TaskController {
 }
